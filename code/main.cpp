fcfd06e32c979dfce8318fea1e82fff8fb0dec42
Andy Dickinson <dickinson.andrew@hotmail.co.uk>
2024-01-10 11:31:41 +0000

    started notes

---
+++ code/main.cpp
@@ -0,0 +1,15 @@
+#include <iostream>
+
+using namespace std;
+
+// argc and argv are not required, however they provide a way to access console parameters
+int main(int argc, char const *argv[]) {
+    /*
+    'cout' is console out
+    '<< endl' is optional, and adds a new line to the output
+    */
+    cout << "Hello!" << endl;
+
+    // 'return' is optional in cpp
+    return 0;
+}
---
+++ cpp_notes.md
@@ -0,0 +1,1 @@
+# <center>CPP Notes</center>
